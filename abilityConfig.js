3ede97f6015898d492a9ce76a69652f4fe12158d
hmongkey12 <khammadaolee@gmail.com>
2023-02-27 20:29:59 -0800

    added collision detection

--- abilityConfig.js
+++ abilityConfig.js
@@ -5,13 +5,13 @@ const loadPumpkinAbilities = function () {
    const ability2 = new abilityEntity("pumpkin_2");
    ability1.cooldownEnd = 4;
    ability1.damage = 10;
-   ability1.width = 10;
-   ability1.height = 20;
+   ability1.width = 100;
+   ability1.height = 200;
    abilityList.push(ability1);
    ability2.cooldownEnd = 10;
    ability2.damage = 50;
-   ability2.width = 30;
-   ability2.height = 10;
+   ability2.width = 100;
+   ability2.height = 200;
    abilityList.push(ability2);
    return abilityList;
 }
@@ -22,13 +22,13 @@ const loadReaperAbilities = function () {
     const ability2 = new abilityEntity("reaper_2");
     ability1.cooldownEnd = 4;
     ability1.damage = 10;
-    ability1.width = 10;
-    ability1.height = 20;
+    ability1.width = 100;
+    ability1.height = 200;
     abilityList.push(ability1);
     ability2.cooldownEnd = 10;
     ability2.damage = 50;
-    ability2.width = 30;
-    ability2.height = 10;
+    ability2.width = 100;
+    ability2.height = 200;
     abilityList.push(ability2);
     return abilityList;
 }
--- utilities.js
+++ utilities.js
@@ -1,3 +1,25 @@
+const JUMP_CONSTANT = 10;
+const MAX_JUMP_HEIGHT = 100;
+const GROUND_Y_POSITION = 0;
+const JUMPS_PER_SECOND = 2;
+const doEntitiesCollide = (firstEntity, secondEntity) => {
+    const firstEntityXposEnd = firstEntity.xPos + firstEntity.width;
+    const secondEntityXposEnd = secondEntity.xPos + secondEntity.width;
+    const firstEntityYposEnd = firstEntity.yPos + firstEntity.height;
+    const secondEntityYposEnd = secondEntity.yPos + secondEntity.height;
+    if (secondEntity.xPos > firstEntityXposEnd) {
+        return false;
+    } else if (secondEntityXposEnd < firstEntity.xPos) {
+        return false;
+    } else if (firstEntityYposEnd < secondEntity.yPos){
+       return false;
+    } else if (firstEntity.yPos > secondEntityYposEnd) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 const handleInput = (gameState, playerId, args) => {
     const movementSpeed = 5;
     if (args.includes("left") && !gameState.connected.get(playerId).isAttacking) {
@@ -37,6 +59,58 @@ const handleInput = (gameState, playerId, args) => {
     }
 }
 
+const handleUpdates = (gameState, playerId) => {
+    let attackStart = gameState.connected.get(playerId).attackStart;
+    let jumpStart = gameState.connected.get(playerId).jumpStart;
+    let jumpEnd = Date.now();
+    let attackEnd = Date.now();
+    let movingEnd = attackEnd;
+    let movingStart = gameState.connected.get(playerId).movingStart;
+    let elapsedAttackTime = Math.floor((attackEnd - attackStart) / 1000);
+    let elapsedMovingTime = Math.floor((movingEnd - movingStart) / 1000);
+    let elapsedJumpingTime = Math.floor((jumpEnd - jumpStart) / 1000);
+    if (gameState.connected.get(playerId).isAttacking && elapsedAttackTime >= .5) {
+        gameState.connected.get(playerId).isAttacking = false;
+        gameState.connected.get(playerId).attackStart = 0;
+        gameState.connected.get(playerId).attackEnd = 0;
+    } else if (gameState.connected.get(playerId).isAttacking) {
+        gameState.connected.forEach((heroEntity) => {
+            if (heroEntity.id != playerId) {
+                gameState.connected.get(playerId).abilities.forEach((ability) => {
+                    if (doEntitiesCollide(heroEntity, ability)) {
+                        heroEntity.health -= 1;
+                    }
+                })
+            }
+        })
+    }
+    if (gameState.connected.get(playerId).isMoving && elapsedMovingTime >= .5) {
+        gameState.connected.get(playerId).isMoving = false;
+        gameState.connected.get(playerId).movingStart = 0;
+        gameState.connected.get(playerId).movingEnd = 0;
+    }
+
+    if (elapsedJumpingTime >= JUMPS_PER_SECOND) {
+        if (gameState.connected.get(playerId).isJumping) {
+            if (gameState.connected.get(playerId).yPos < MAX_JUMP_HEIGHT) {
+                gameState.connected.get(playerId).yPos += JUMP_CONSTANT;
+            } else {
+                gameState.connected.get(playerId).isJumping = false;
+                gameState.connected.get(playerId).isFalling = true;
+            }
+        } else if (gameState.connected.get(playerId).isFalling) {
+            if (gameState.connected.get(playerId).yPos > GROUND_Y_POSITION) {
+                gameState.connected.get(playerId).yPos -= JUMP_CONSTANT;
+            } else {
+                gameState.connected.get(playerId).yPos = GROUND_Y_POSITION;
+                gameState.connected.get(playerId).isFalling = false;
+            }
+        }
+    }
+}
+
+
+
 module.exports = {
-    handleInput
+    handleInput, handleUpdates
 }
